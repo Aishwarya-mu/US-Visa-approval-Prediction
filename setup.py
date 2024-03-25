7a967e4fd6eeff05825a1109e0e3a488fd4731aa
Aishwarya-mu <aishubhanu96@gmail.com>
2024-03-25 14:53:19 -0400

    added constants and pipeline components

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Aishwarya",
+    author_email="aishwarya.mandagere@gmail.com",
+    packages=find_packages()
+)
